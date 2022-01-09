837328eb08251ff3d912833999c77429b7e8b725
Abdul Rafi <arafi6624@bths.edu>
2022-01-09 18:03:40 -0500

    Spelling Mistake Corrected

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+"</mark>,<br>at an interest rate of <mark>"+rate+"</mark>%.<br>You will recieve an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+year+"</mark>."
+    document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+"</mark>,<br>at an interest rate of <mark>"+rate+"</mark>%.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+year+"</mark>."
 }
         
 function updateRate(){
